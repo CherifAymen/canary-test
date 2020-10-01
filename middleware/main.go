a77631309cec8ff8bb0210ae1d2f0b96b8e5e8d9
CherifAymen <aymen.cherif@supcom.tn>
2020-10-01 18:30:16 +0200

    make an ERROR

--- middleware/main.go
+++ middleware/main.go
@@ -49,35 +49,47 @@ func sentimentHandler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	if len(reqBody) != 0 {
-		//var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-		req, err := http.NewRequest("POST", fullURL, bytes.NewBuffer(reqBody))
-		req.Header.Set("Content-Type", "application/json")
+	
+		rand.Seed(time.Now().UnixNano())
+		n := rand.Intn(10)
 
-		t := time.Now()
-		clock := fmt.Sprintf("[%d-%02d-%02d  %02d:%02d:%02d]",
-			t.Year(), t.Month(), t.Day(),
-			t.Hour(), t.Minute(), t.Second())
 
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			panic(err)
+		if n == 0 {
+			code = http.StatusInternalServerError
+			msg = "ERROR: Something, somewhere, went wrong!\n"
+			w.WriteHeader(code)
+			io.WriteString(w, msg)
+		} else {
+			
+			//var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+			req, err := http.NewRequest("POST", fullURL, bytes.NewBuffer(reqBody))
+			req.Header.Set("Content-Type", "application/json")
+
+			t := time.Now()
+			clock := fmt.Sprintf("[%d-%02d-%02d  %02d:%02d:%02d]",
+				t.Year(), t.Month(), t.Day(),
+				t.Hour(), t.Minute(), t.Second())
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+
+			fmt.Println(req.Host+" -- "+clock+" \""+req.Method, r.URL.Path, " ", resp.StatusCode, " - ")
+
+			body, err := ioutil.ReadAll(resp.Body)
+
+			if err != nil {
+				panic(err)
+			}
+
+			var sentiment Sentiment
+			json.Unmarshal(body, &sentiment)
+			sentiment.Version = "go"
+			json.NewEncoder(w).Encode(sentiment)
 		}
-		defer resp.Body.Close()
-
-		fmt.Println(req.Host+" -- "+clock+" \""+req.Method, r.URL.Path, " ", resp.StatusCode, " - ")
-
-		body, err := ioutil.ReadAll(resp.Body)
-
-		if err != nil {
-			panic(err)
-		}
-
-		var sentiment Sentiment
-		json.Unmarshal(body, &sentiment)
-		sentiment.Polarity += 100
-		sentiment.Version = "go"
-		json.NewEncoder(w).Encode(sentiment)
 	}
 }
 
