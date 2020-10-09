15a65ba5137915fdaaed26587e83294695a7d2bd
CherifAymen <aymen.cherif@supcom.tn>
2020-10-09 17:28:56 +0200

    update main.go

--- load.sh
+++ load.sh
@@ -8,7 +8,7 @@ ADDR=$(kubectl get svc istio-ingressgateway -n istio-system --output jsonpath="{
 while true; do \
     curl  http://$ADDR/sentiment  -H "Content-type: application/json" \
     -d '{"sentence": "I love microservices"}' \
-    -s -w "\t Time: %{time_total}s \t Status: %{http_code} \n" -o -; \
+    -s -w "\t Time: %{time_total}s \t Status: %{http_code} \n \n" -o - ; \
     sleep 2;\
     done
 
